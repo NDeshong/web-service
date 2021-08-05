932970dec9ae7d7ad177285ef224d45982505337
Nicole Deshong <nicoleanjalii@gmail.com>
2021-08-05 11:19:57 -0700

    loops through films and adds template

--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 <head>
 <title>Bond Web Service Demo</title>
 <style>
-body {font-family:georgia;}
+	body {font-family:georgia;}
+	.film
+	{
+		border:1px solid #E77DC2;
+		border-radius: 5px;
+		padding: 5px;
+		margin-bottom:5px;
+		position:relative;	
+	}
+
+	.pic
+	{
+		position:absolute;
+		right:10px;
+		top:10px;
+	}
+
 </style>
 <script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
@@ -39,14 +55,56 @@ function toConsole(data)
 function bondJSON(data){
 	//here is how I see data returned via the console
 	console.log(data);
+	//identifies the type of data returned
+	$('#filmtitle').html(data.title);
+
+	//clears other clicked films
+	$('#films').html('');
+
+	//loops through films and add template
+	$.each(data.films,function(i,item){
+		let myFilm = bondTemplate(item);
+
+		$('<div></div>').html(myFilm).appendTo('#films');
+
+	});
+
+
+
 
 	// this loads data on page but bunched up
 	//$("#output").text(JSON.stringify(data));
 
+	//this creates a map of the JSON on our page
+	/*
 	let myData = JSON.stringify(data,null,4);
-
 	myData = "<pre>" + myData + "</pre>";
 	$("#output").html(myData);
+	*/
+}
+
+function bondTemplate(film){
+
+	return `
+	
+			<div class="film">
+			<b>Film: </b>${film.Film}<br />
+			<b>Title: </b>${film.Title}<br />
+			<b>Year: </b>${film.Year}<br />
+			<b>Director: </b>${film.Director}<br />
+			<b>Producers: </b>${film.Producers}<br />
+			<b>Writers: </b>${film.Writers}<br />
+			<b>Composer: </b>${film.Composer}<br />
+			<b>Bond: </b>${film.Bond}<br />
+			<b>Budget: </b>${film.Budget}<br />
+			<b>BoxOffice: </b>${film.BoxOffice}<br />
+			<div class = "pic"><img src ="thumbnails/${film.Image}" /></div>
+		</div>
+	
+
+
+	`;
+
 }
 
 </script>
@@ -57,7 +115,20 @@ function bondJSON(data){
 		<a href="box" class="category">Bond Films By International Box Office Totals</a>
 		<h3 id="filmtitle">Title Will Go Here</h3>
 		<div id="films">
-			<p>Films will go here</p>
+		<!--	<div class="film">
+			<b>Film: </b>1<br />
+			<b>Title: </b>Dr. No<br />
+			<b>Year: </b>1962<br />
+			<b>Director: </b>Terence Young<br />
+			<b>Producers: </b>Harry Saltzman and Albert R. Broccoli<br />
+			<b>Writers: </b>Richard Maibaum, Johanna Harwood and Berkely Mather<br />
+			<b>Composer: </b>Monty Norman<br />
+			<b>Bond: </b>Sean Connery<br />
+			<b>Budget: </b>$1,000,000.00<br />
+			<b>BoxOffice: </b>$59,567,035.00<br />
+			<div class = "pic"><img src ="thumbnails/dr-no.jpg"></div>
+		</div>
+		-->
 		</div>
 		<div id="output">Results go here</div>
 	</body>
